2db0d1c79cbec27c2b37d8513bba3861cd2cc3fa
SungyupKim <sungyupv@gmail.com>
2022-02-06 18:31:31 +0900

    get user info.

--- src/main/java/com/cloud/pass/userinfo/UserInfoController.java
+++ src/main/java/com/cloud/pass/userinfo/UserInfoController.java
@@ -1,7 +1,8 @@
 package com.cloud.pass.userinfo;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
+import java.util.List;
+import java.util.Map;
+
 import org.keycloak.KeycloakPrincipal;
 import org.keycloak.KeycloakSecurityContext;
 import org.keycloak.representations.AccessToken;
@@ -12,35 +13,27 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class UserInfoController {
 	@ResponseBody
 	@GetMapping(value = "/userInfo", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Object> getUserInfo() {
-		JSONArray jsonArr = new JSONArray();
-
-//		for (ServiceInfo info : serviceInfoService.getServiceInfo(cluster, namespace).getServicesList()) {
-//			String output = JsonFormat.printer().print(info);
-//			JSONObject jsonObject = new JSONObject(output);
-//			jsonArr.put(jsonObject);
-//		}
-
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
 		KeycloakPrincipal principal = (KeycloakPrincipal) auth.getPrincipal();
-
 		KeycloakSecurityContext session = principal.getKeycloakSecurityContext();
 		AccessToken accessToken = session.getToken();
 		String username = accessToken.getPreferredUsername();
-		String emailID = accessToken.getEmail();
-		String lastname = accessToken.getFamilyName();
-		String firstname = accessToken.getGivenName();
 		String realmName = accessToken.getIssuer();
-		AccessToken.Access realmAccess = accessToken.getRealmAccess();
-		JSONObject jsonRoot = new JSONObject();
-
-		jsonRoot.put("pods", jsonArr);
-		System.out.println(jsonRoot);
-		return new ResponseEntity<Object>(username, HttpStatus.OK);
+		String client = accessToken.getIssuedFor();
+		Map<String, Object> obj = accessToken.getOtherClaims();
+		List<String> groups = null;
+		if (obj.containsKey("groups")) {
+			groups = (List<String>) obj.get("groups");
+		}
+		UserInfo info = new UserInfo(username, groups, client);
+		return new ResponseEntity<Object>(info, HttpStatus.OK);
 	}
 }
