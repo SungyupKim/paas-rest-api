bbabead0fd432719f867df6c74929209104a6f98
SungyupKim <sungyupv@gmail.com>
2022-02-06 19:16:10 +0900

    UserInfo class.

---
+++ src/main/java/com/cloud/pass/userinfo/UserInfo.java
@@ -0,0 +1,19 @@
+package com.cloud.pass.userinfo;
+
+import java.util.List;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@AllArgsConstructor
+public class UserInfo {
+	String name;
+	List<String> groups;
+	String client;
+}
